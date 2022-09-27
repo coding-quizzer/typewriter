6bcce437bec5c0497bb18825171fba05a88b33a3
William Gadd <wgadd@sasktel.net>
2022-09-27 18:00:55 +0000

    Added extra newline in setTimeout() function so that the newline character is printed after everything else

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,9 @@
-const sentence = "Welcome here, how has your day been?";
+const sentence = "Welcome, how has your day been?";
 let timeoutDelay = 0;
 for (const char of sentence) {
 
   setTimeout(() => process.stdout.write(char), timeoutDelay);
   timeoutDelay += 50;
 }
+
+setTimeout(() => process.stdout.write('\n'), timeoutDelay + 1);
